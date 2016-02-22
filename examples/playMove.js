dc1ec9e68cb52e12c8c1db1cab2c08ea9a93d84e
lmolloy <liam_j_molloy@hotmail.com>
2016-02-21 20:24:01 -0800

    firebase queues

--- examples/playMove.js
+++ examples/playMove.js
@@ -1,7 +1,15 @@
 // Push a PlayMove message into the queue.
 var config = require('../lib/config');
-var match = require("../test/testData/validMatch");
-var playMove = require("../test/testData/validRequestPlayMoveSchema");
+//var match = require("../test/testData/validMatch");
+//var playMove = require("../test/testData/validRequestPlayMoveSchema");
 var serviceFirebase = require('../lib/services/firebase');
-serviceFirebase.Set(config.MatchEndPoint, match._id, match);
-serviceFirebase.Push(config.QueueEndPoint, config.PlayMoveEndPoint, playMove);
+//serviceFirebase.Set(config.MatchEndPoint, match._id, match);
+
+var move = {
+    cardId: "1",
+    matchId: "1",
+    userId: "1"
+};
+
+serviceFirebase.Push(config.QueueEndPoint, config.PlayMoveEndPoint, move);
+return 0;
